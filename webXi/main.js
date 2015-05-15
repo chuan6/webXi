ad25314a3f64a24702f925ef7570238ff6b8d775
chuan6 <chuan6.dev@gmail.com>
2015-05-15 11:42:21 -0500

    webXi/: add query related utility functions.

--- webXi/main.js
+++ webXi/main.js
@@ -1,11 +1,4 @@
-var body = document.getElementsByTagName("body")[0];
-
-function appendDOM(target, tag, text) {
-    target.appendChild(
-        document.createElement(tag).appendChild(
-            document.createTextNode(text)));
-}
-
+// make side-effect over an array
 function iterator(func) {
     return (
         function (arr) {
@@ -14,6 +7,51 @@ function iterator(func) {
             }
         }
     );
+}
+
+// evaluate over an array
+function reduce(fn, init, arr) {
+    var env = init;
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        env = fn(env, arr[i]);
+    }
+    return env;
+}
+
+// table-to-table query
+function do_query(q, table) {
+    var do_row = function (env, x) {
+        var v = q(x);
+        if (v)
+            env.push(v);
+        return env;
+    };
+    return reduce(do_row, [], table);
+}
+
+// provide SELECT and WHERE
+function query(cols, pred) {
+    return function (row) {
+        var n = cols.length;
+        var ret, i;
+        if (pred(row) == true) {
+            ret = [];
+            for (i = 0; i < n; i++) {
+                ret.push(row[cols[i]]);
+            }
+        }
+        // else, ret is undefined
+        return ret;
+    };
+}
+
+var body = document.getElementsByTagName("body")[0];
+
+function appendDOM(target, tag, text) {
+    target.appendChild(
+        document.createElement(tag).appendChild(
+            document.createTextNode(text)));
 }
 
 function do_visit_item(x) {
@@ -26,6 +64,21 @@ function do_history_item(x) {
         {url: url}, iterator(do_visit_item));
 }
 
+var test_query_urls = query(
+    ["url", "visitCount"],
+    function (row) {
+        return row["visitCount"] > 1;
+    }
+);
+
+var do_test_query_urls = function (table) {
+    var hitems = do_query(test_query_urls, table);
+    var i, n = hitems.length;
+    for (i = 0; i < n; i++) {
+        appendDOM(body, "div", JSON.stringify(hitems[i]));
+    }
+};
+
 chrome.history.search(
-    {text: "", maxResults: 1000000}, iterator(do_history_item));
+    {text: "", maxResults: 1000000}, do_test_query_urls);
 
