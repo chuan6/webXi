28573f4704f7f59eeb1f64549024fa66c5a45618
chuan6 <chuan6.dev@gmail.com>
2015-05-22 23:27:28 -0500

    webXi/: combine data with representation, see function consURL.
    And thus simplifies previous code.

--- webXi/main.js
+++ webXi/main.js
@@ -1,112 +1,111 @@
-function cons_colgroup(data) {
-    var n = data.cols.length,
-        colgroup = document.createElement("colgroup"),
-        i, col;
+var NA = "?"; //denote data that is not available
 
-    for (i = 0; i < n; i++) {
-        col = document.createElement("col");
-        col.id = "c" + i;
-        colgroup.appendChild(col);
-    }
-    return colgroup;
+function getData(obj, property) {
+    var x = obj[property];
+    return x? x : NA;
 }
 
-function cons_thead(data) {
-    var v = data.cols,
-        n = v.length,
-        thead = document.createElement("thead"),
-        tr = document.createElement("tr"),
-        i, th;
+function consURL(hx) {
+    if (!hx.url)
+        return false;
+    //hx has property "url"
 
-    for (i = 0; i < n; i++) {
-        th = document.createElement("th");
-        th.id = "h" + i;
-        th.textContent = v[i];
-        tr.appendChild(th);
-    }
-    thead.appendChild(tr);
-    return thead;
-}
+    //convert values of missing properties to NAs
+    hx.lastVisitTime = getData(hx, "lastVisitTime");
+    hx.visitCount = getData(hx, "visitCount");
+    hx.typedCount = getData(hx, "typedCount");
 
-function cons_tbody(data) {
-    var ncols = data.cols.length,
-        nrows = data.rows.length,
-        tbody = document.createElement("tbody"),
-        i, j, x, tr, td;
+    //attach corresponding html element: tr
+    hx.htmlElem = function() {
+        var td, tr = document.createElement("tr");
+        
+        td = document.createElement("td");
+        td.textContent = hx.url;
+        tr.appendChild(td);
 
-    for (i = 0; i < nrows; i++) {
-        tr = document.createElement("tr");
-        x = data.rows[i];
-        for (j = 0; j < ncols; j++) {
-            td = document.createElement("td");
-            if (x[j] + 0 === x[j]) { //x[j] is a number
-                td.textContent = String(x[j]);
-                td.style.textAlign = "right"; //left-align a number
-            } else {
-                td.textContent = String(x[j]);
-            }
-            tr.appendChild(td);
-        }
-        tbody.appendChild(tr);
-        data.rows[i].html = tr;
-    }
-    return tbody;
+        td = document.createElement("td");
+        td.textContent = String(hx.lastVisitTime);
+        tr.appendChild(td);
+        
+        td = document.createElement("td");
+        td.textContent = String(hx.visitCount);
+        td.style.textAlign = "right";
+        tr.appendChild(td);
+
+        td = document.createElement("td");
+        td.textContent = String(hx.typedCount);
+        td.style.textAlign = "right";
+        tr.appendChild(td);
+
+        return tr;
+    }();
+
+    return hx;
 }
 
 function batchAddEventListener(elems, event, handler) {
     var n = elems.length, i;
     for (i = 0; i < n; i++) {
-        elems[i].addEventListener(event, handler);
+	elems[i].addEventListener(event, handler);
     }
 }
 
-var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+var millisecondsPerDay = 1000 * 60 * 60 * 24;
 var now = (new Date()).getTime();
-var oneWeekAgo = now - millisecondsPerWeek;
-var oneMonthAgo = now - millisecondsPerWeek*4;
+var oneDayAgo = now - millisecondsPerDay;
+var oneWeekAgo = now - millisecondsPerDay * 7;
+var oneMonthAgo = now - millisecondsPerDay * 28;
 chrome.history.search(
     {
-        text: "",
-        startTime: oneWeekAgo,
-        endTime: now,
-        maxResults: 1000000
+	text: "",
+	startTime: oneDayAgo,
+	endTime: now,
+	maxResults: 1000000
     },
-    function (v) {
-        var cs = ["id", "url", "title", "lastVisitTime", "visitCount", "typedCount"],
-            rs = [],
-            m = v.length, n = cs.length,
-            i, j, x, y, r, data,
-            table, theads, tbody;
+    function (hv) {
+        var data = [];
+        var n = hv.length;
+        var i;
 
-        for (i = 0; i < m; i++) {
-            r = [];
-            x = v[i];
+        for (i = 0; i < n; i++) {
+            data.push(consURL(hv[i]));
+        }
+
+        document.body.appendChild(function() {
+            var table = document.createElement("table"),
+                colgroup = document.createElement("colgroup"),
+                thead = document.createElement("thead"),
+                tbody = document.createElement("tbody");
+            var col, tr, th, td;
+            var cs = ["url", "lastVisitTime", "visitCount", "typedCount"],
+                m = cs.length;
+            var j; //index for rows
+
+            tr = document.createElement("tr");
+            for (i = 0; i < m; i++) {
+                col = document.createElement("col");
+                col.id = "c" + i;
+                colgroup.appendChild(col);
+
+                th = document.createElement("th");
+                th.id = "h" + i;
+                th.textContent = cs[i];
+                tr.appendChild(th);
+            }
+            thead.appendChild(tr);
+            table.appendChild(colgroup);
+            table.appendChild(thead);
+
             for (j = 0; j < n; j++) {
-                y = x[cs[j]];
-                if (y) {
-                    r.push(y);
-                } else {
-                    r.push("?"); // use ? to fill empty slots
-                }
-            } // r is ready
-            rs.push(r);
-        } // rs is ready
-        data = {cols: cs, rows: rs};
+                tbody.appendChild(data[j].htmlElem);
+            }
+            table.appendChild(tbody);
 
-        table = document.createElement("table");
-        //table.style.border = "1px solid grey";
-        table.style.borderCollapse = "collapse";
-        table.appendChild(cons_colgroup(data));
-        table.appendChild(cons_thead(data));
-        table.appendChild(cons_tbody(data));
-        var theads = table.getElementsByTagName("thead")[0].getElementsByTagName("tr")[0].children;
-        batchAddEventListener(theads, "click", function() {
-            var col = document.getElementById("c" + this.id.slice(1)),
-                color_off = "white",
-                color_on = "#B3D4FC",
-                flag = "is-selected", on = "1", off = "0";
-            console.log(col.getAttribute(flag));
-            switch (col.getAttribute(flag)) {
+            batchAddEventListener(table.getElementsByTagName("th"), "click", function() {
+                var col = document.getElementById("c" + this.id.slice(1));
+                var flag = "is-selected", on = "1", off = "0",
+                    color_on = "#B3D4FC", color_off = "white";
+                switch (col.getAttribute(flag)) {
                 case off:
                     col.setAttribute(flag, on);
                     col.style.backgroundColor = color_on;
@@ -118,32 +117,15 @@ chrome.history.search(
                 default:
                     col.setAttribute(flag, on);
                     col.style.backgroundColor = color_on;
-            }
-        });
-        document.body.appendChild(table);
+                }
+            });
 
-        data.rows = data.rows.sort(function(a, b) {
-            var x = a[4], y = b[4];
-            if (x === y) return 0;
-            if (x === "?") {
-                //console.log("x === ?");
-                return 1;
-            }
-            if (y === "?") {
-                //console.log("y === ?");
-                return -1;
-            }
-            return y - x;
-        });
-        console.log("#rows: " + data.rows.length);
-        tbody = document.createElement("tbody");
-        for (i = 0; i < m; i++) {
-            //console.log(data.rows[i]);
-            tbody.appendChild(data.rows[i].html);
-        }
-        table.removeChild(table.childNodes[2]);
-        table.appendChild(tbody);
-    });
+            table.style.borderCollapse = "collapse";
+
+            return table;
+        }());
+    }
+);
 
 // TODO sorting by a chosen column
 
